1b8447e5eea358e342d8c712ce57a6248f3f4e2d
Kadenaa <chainedlink2@gmail.com>
2018-07-07 17:15:01 -0500

    Added GetBlackboardString Node

---
+++ Assets/Packages/Dialogues/Scripts/Classes/DialogueNodes/GetBlackboardString.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GetBlackboardString : DialogueNode, IValueNode<string> {
+	public string variableName;
+
+	public override void DetailEditorGUI() {
+		variableName = EditorGUILayout.TextField("Variable Name", variableName);
+	}
+
+	public string GetValue() {
+		object varaible = Blackboard.Instance[variableName];
+		if (varaible != null) {
+			return varaible.ToString();
+		} else {
+			return null;
+		}
+	}
+}
